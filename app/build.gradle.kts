2d1c9120079a419aaeabead342e4988fdcd555ac
Gerardo <romero.gerardo.ds@gmail.com>
2026-09-05 20:43:20 -0300

    feat(i2): crear repositorio de carreras y cliente retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,5 +35,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.8.5")
     debugImplementation("androidx.compose.ui:ui-tooling")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 }
 
